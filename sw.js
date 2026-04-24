128c1f6992ceddb808c7904e32c9af72d7e9d04b
booboopoint-arch <booboopoint-arch@users.noreply.github.com>
2026-04-24 20:44:54 +0900

    sw.jsキャッシュ更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hachibeipOS-v1'
+const CACHE = 'hachibeipOS-20260424'
 const ASSETS = ['pos_menu.html', 'manifest.json', 'epos.js', 'icon-180.png', 'icon-192.png', 'icon-512.png']
 
 self.addEventListener('install', e => {
